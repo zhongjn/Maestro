bfcc68de44cecf9d1a79efac662dbafe3d4d87e0
zhongjn <2389206378@qq.com>
2019-07-25 20:07:14 +0800

    implement random step method

--- search_base.h
+++ search_base.h
@@ -3,23 +3,32 @@
 #include <memory>
 #include "game_base.h"
 #include <cassert>
-
+#include <random>
 namespace Maestro {
     using namespace std;
 
     template<typename TGame>
     class IMonteCarloSearch {
+    protected:
+        minstd_rand _rnd_eng;
     public:
 
         struct MoveVisit {
             Move<TGame> move;
             int visit_count;
         };
+
+        struct MoveVisitProb {
+            Move<TGame> move;
+            float p;
+        };
+
         virtual void simulate(int k) = 0;
         virtual vector<MoveVisit> get_moves() const = 0;
         virtual float get_value(Color color) const = 0;
         virtual TGame get_game_snapshot() const = 0;
         virtual void move(Move<TGame> move) = 0;
+
         void move_best() {
             auto moves = get_moves();
             int max_visit = -1;
@@ -31,5 +40,48 @@ namespace Maestro {
                 }
             }
         }
+
+        vector<MoveVisitProb> get_moves_prob(float temp) const {
+            auto moves = get_moves();
+            vector<float> visit;
+            float sum = 0;
+            
+            vector<MoveVisitProb> move_prob;
+
+            for (auto& mv : moves) {
+                float v = pow(mv.visit_count, 1 / temp);
+                sum += v;
+                move_prob.push_back(MoveVisitProb{ mv.move, v });
+            }
+
+            // normalize
+            for (auto& mv : move_prob) {
+                mv.p /= sum;
+            }
+
+            return move_prob;
+        }
+
+        void move_random(float temp) {
+            assert(temp >= 0.0f);
+            uniform_real_distribution<float> dist;
+            float r = dist(_rnd_eng);
+
+            auto moves = get_moves_prob();
+            assert(moves.size() > 0);
+
+            float accum = 0;
+            int index = 0;
+            for (auto& move : moves) {
+                accum += move.p;
+                if (accum > r) {
+                    break;
+                }
+                index++;
+            }
+
+            move(moves[index].move);
+        }
+
     };
 }
