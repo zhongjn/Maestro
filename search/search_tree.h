e0212efed79a67dd87802b2e019ef1c8fffc7fee
xipotatonium <xipotatonium@outlook.com>
2019-07-26 16:58:53 +0800

    fix diri

--- search/search_tree.h
+++ search/search_tree.h
@@ -36,12 +36,12 @@ namespace Maestro {
 			return ret;
 		}
 
-		MCTSNode* select_best_with_diri(float kucb, float eta, float alpha) {
+		MCTSNode* select_best_with_diri(float kucb, const vector<float>& noise) {
 			MCTSNode* ret = nullptr;
 			float max;
-			vector<float> noise = rand_dirichlet(m_children.size(), alpha);
 			for (int i = 0; i < m_children.size(); ++i) {
-				float ucb = m_children[i]->cal_UCB(kucb, eta, noise[i]);
+                // 参数: epsilon
+				float ucb = m_children[i]->cal_UCB(kucb, noise[i]， 0.25);
 				if (ret == nullptr) {
 					max = ucb;
 					ret = m_children[i];
@@ -104,13 +104,18 @@ namespace Maestro {
 	template<typename TGame>
 	class MonteCarloTreeSearch final : public IMonteCarloSearch<TGame> {
 	public:
+        // 参数: kucb
 		MonteCarloTreeSearch(TGame* init, float kucb, IEvaluator<TGame>* evaluator) {
 			m_root = new MCTSNode<TGame>(nullptr, MovePrior<TGame>(), init);
 			m_root_game = TGame(*init);
 			m_kucb = kucb;
 			m_evaluator = evaluator;
+            
+            m_root->expand(m_evaluator->evaluate(m_root_game));
+            root_expand2();
 		}
 
+        // 参数: k次迭代
 		void simulate(int k) {
 			for (int i = 0; i < k; ++i) {
 				MCTSNode<TGame>* pcur = m_root;
@@ -118,7 +123,11 @@ namespace Maestro {
 					if (!pcur->expanded()) {
 						pcur->expand(m_evaluator->evaluate(*pcur->m_game));
 					}
-					pcur = pcur->select_best(m_kucb);
+                    if (pcur == m_root) {
+                        pcur = pcur->select_best_with_diri(m_kucb, m_noise);
+                    } else {
+					    pcur = pcur->select_best(m_kucb);
+                    }
 				}
 			}
 		}
@@ -157,112 +166,53 @@ namespace Maestro {
 				// move doesn't exist
 				throw;
 			}
-			vector<Move<TGame>> moves = m_root->m_game->get_all_legal_moves();
-			int children_size = m_root->m_children.size();
-
-			for (Move<TGame>& m : moves) {
-				bool found = false;
-				for (int i = 0; i < children_size; ++i) {
-					if (m_root->m_children[i]->m_move.move == m) {
-						found = true;
-						break;
-					}
-				}
-				if (!found) {
-					MCTSNode<TGame>* new_node = 
-						new MCTSNode<TGame>(m_root, MovePrior<TGame>{m, 0}, new TGame(m_root_game));
-					new_node->m_game->move(m);
-					m_root->m_children.emplace_back(new_node);
-				}
-			}
+            root_expand2();
 		}
 	private:
+        // Fully expand root and generate dirichlet noise
+        void root_expand2() {
+            vector<Move<TGame>> moves = m_root_game->get_all_legal_moves();
+            int children_size = m_root->m_children.size();
+
+            for (Move<TGame>& m : moves) {
+                bool found = false;
+                for (int i = 0; i < children_size; ++i) {
+                    if (m_root->m_children[i]->m_move.move == m) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    MCTSNode<TGame>* new_node =
+                        new MCTSNode<TGame>(m_root, MovePrior<TGame>{m, 0}, new TGame(m_root_game));
+                    new_node->m_game->move(m);
+                    m_root->m_children.emplace_back(new_node);
+                }
+            }
+            // 参数: Dir(0.03)
+            m_noise = rand_dirichlet(m_root->m_children.size(), 0.03);
+        }
+
+        std::vector<float> rand_dirichlet(int n, float concentration)
+        {
+            std::vector<float> ret = std::vector<float>(n, 0);
+            std::gamma_distribution<float> gamma(concentration, 1);
+            float sum = 0;
+            for (int i = 0; i < n; ++i) {
+                ret[i] = gamma(_rnd_eng);
+                sum += ret[i];
+            }
+            for (int i = 0; i < n; ++i) {
+                ret[i] = ret[i] / sum;
+            }
+            return ret;
+        }
+
 		MCTSNode<TGame>* m_root;
 		IEvaluator<TGame>* m_evaluator;
 		TGame m_root_game;
 		float m_kucb;
+        vector<float> m_noise;
 	};
 }
 
-// https://www.cnblogs.com/yeahgis/archive/2012/07/15/2592698.html
-float rand_gamma(float alpha) {
-	float u, v;
-	float x, y;
-	float b, c;
-	float w, z;
-	bool accept = false;
-	float t;
-
-	if (alpha > 1.0) {
-		/* Best's rejection algorithm XG for gamma random variates (Best, 1978) */
-		b = alpha - 1;
-		c = 3 * alpha - 0.75;
-		do {
-			u = rand() / RAND_MAX;
-			v = rand() / RAND_MAX;
-
-			w = u * (1 - u);
-			y = sqrt(c / w) * (u - 0.5);
-			x = b + y;
-			if (x >= 0) {
-				z = 64 * w * w * w * v * v;
-				float zz = 1 - 2 * y * y / x;
-				if (z - zz < 1e-10) {
-					accept = true;
-				} else {
-					accept = false;
-				}
-				if (!accept) {
-					float logz = log(z);
-					float zzz = 2 * (b * log(x / b) - y);
-					if (logz - zzz < 1e-10) {
-						accept = true;
-					} else {
-						accept = false;
-					}
-				}
-			}
-		} while (!accept);
-		return x;
-	} else if (alpha == 1.0) {
-		x = rand() / RAND_MAX;
-		// return -log(1 - x);
-		return -log(x);
-	} else if (alpha < 1.0) {
-		float dv = 0.0;
-		float b = (exp(1.0) + alpha) / exp(1.0);
-		do {
-			float u1 = rand() / RAND_MAX;
-			float p = b * rand() / RAND_MAX;
-			float y;
-			if (p > 1) {
-				y = -log((b - p) / alpha);
-				if (u1 < pow(y, alpha - 1)) {
-					dv = y;
-					break;
-				}
-			} else {
-				y = pow(p, 1 / alpha);
-				if (u1 < exp(-y)) {
-					dv = y;
-					break;
-				}
-			}
-		} while (1);
-		return dv;
-	}
-	return -1;
-}
-
-std::vector<float> rand_dirichlet(int n, float concentration) {
-	std::vector<float> ret = std::vector<float>(n, 0);
-	float sum = 0;
-	for (int i = 0; i < n; ++i) {
-		ret[i] = rand_gamma(concentration);
-		sum += ret[i];
-	}
-	for (int i = 0; i < n; ++i) {
-		ret[i] = ret[i] / sum;
-	}
-	return ret;
-}
