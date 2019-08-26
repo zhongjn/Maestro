e81af4eb83b3aa42b860724004e32f7912ef8d14
zhongjn <2389206378@qq.com>
2019-08-26 16:48:23 +0800

    turn the CI warning on

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 mkdir build
 cd build
 cmake .. -G "Visual Studio 15 2017" -A Win32
-MSBuild.exe Maestro.sln /p:Configuration=Release /p:Platform=Win32 /clp:ErrorsOnly
+MSBuild.exe Maestro.sln /p:Configuration=Release /p:Platform=Win32
 cd Release
 TestEntry.exe
